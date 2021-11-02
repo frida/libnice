2b2c8fa59754499c4532e44e2bef4f94f1d00ee9
Ole André Vadla Ravnås <oleavr@gmail.com>
2021-11-02 23:39:30 +0100

    agent: Handle failure to create TCP TURN socket
    E.g. if the server address is IPv6 but the local system cannot reach it,
    so connect() fails right away.

--- agent/agent.c
+++ agent/agent.c
@@ -2726,8 +2726,7 @@ priv_add_new_candidate_discovery_turn (NiceAgent *agent,
 
   if (turn->type == NICE_RELAY_TYPE_TURN_UDP) {
     if (agent->use_ice_udp == FALSE || turn_tcp == TRUE) {
-      g_slice_free (CandidateDiscovery, cdisco);
-      return;
+      goto skip;
     }
     if (agent->compatibility == NICE_COMPATIBILITY_GOOGLE) {
       NiceAddress addr = nicesock->addr;
@@ -2763,17 +2762,18 @@ priv_add_new_candidate_discovery_turn (NiceAgent *agent,
     /* Ignore tcp candidates if we disabled ice-tcp */
     if ((agent->use_ice_udp == FALSE && reliable_tcp == FALSE) ||
         (agent->use_ice_tcp == FALSE && reliable_tcp == TRUE)) {
-      g_slice_free (CandidateDiscovery, cdisco);
-      return;
+      goto skip;
     }
 
     if (turn_tcp == FALSE) {
-      g_slice_free (CandidateDiscovery, cdisco);
-      return;
+      goto skip;
     }
 
     cdisco->nicesock = agent_create_tcp_turn_socket (agent, stream,
         component, nicesock, &turn->server, turn->type, reliable_tcp);
+    if (!cdisco->nicesock) {
+      goto skip;
+    }
 
     nice_component_attach_socket (component, cdisco->nicesock);
   }
@@ -2812,6 +2812,11 @@ priv_add_new_candidate_discovery_turn (NiceAgent *agent,
       agent, cdisco);
   agent->discovery_list = g_slist_append (agent->discovery_list, cdisco);
   ++agent->discovery_unsched_items;
+
+  return;
+
+skip:
+  g_slice_free (CandidateDiscovery, cdisco);
 }
 
 NICEAPI_EXPORT guint
